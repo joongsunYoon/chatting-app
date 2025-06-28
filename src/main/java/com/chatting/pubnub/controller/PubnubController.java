95b705eb20f7957849277f43c8eb66f47a3f5dd9
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-06-28 18:34:38 +0900

    feat: 메서지 전송 , 수신 api 추가

--- src/main/java/com/chatting/pubnub/controller/PubnubController.java
+++ src/main/java/com/chatting/pubnub/controller/PubnubController.java
@@ -1,5 +1,8 @@
 package com.chatting.pubnub.controller;
 
+import com.chatting.global.exception.GlobalExceptionHandler.ApiResponse;
+import com.chatting.pubnub.config.PubNubConfig;
+import com.chatting.pubnub.service.PubnubPublishService;
 import com.pubnub.api.PubNub;
 import com.pubnub.api.PubNubException;
 import com.pubnub.api.callbacks.PNCallback;
@@ -8,30 +11,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequestMapping("/api/pubnub")
 @RequiredArgsConstructor
 public class PubnubController {
 
     private final PubNub pubNub;
+    private final PubnubPublishService pubnubPublishService;
 
-    @PostMapping("/send")
-    public ResponseEntity<String> sendMessage(@RequestParam String message) {
-        pubNub.publish()
-                .channel("chat-room-1")
-                .message("Hello from Spring!")
-                .async(new PNCallback<PNPublishResult>() {
-                    @Override
-                    public void onResponse(PNPublishResult result, PubNubException e) {
-                        if (e == null) {
-                            System.out.println("전송 성공! 타임토큰: " + result.getTimetoken());
-                        } else {
-                            System.err.println(" 전송 실패: " + e.getMessage());
-                        }
-                    }
-                });
-
-        return ResponseEntity.ok("전송 요청 완료");
+    @PostMapping("start")
+    public ResponseEntity<ApiResponse<String>> start() {
+        pubnubPublishService.init();
+        return ResponseEntity.ok(new ApiResponse<>(200 , "채팅연결 성공" , null));
     }
+
+    @PostMapping()
+    public ResponseEntity<ApiResponse<String>> sendMessage(@RequestParam String message) {
+
+        pubnubPublishService.sendMessage("test_channel", message);
+        return ResponseEntity.ok(new ApiResponse<>(200 , "전송 성공" , null));
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<String>> receiveMessage(@RequestParam String channel) {
+
+        pubnubPublishService.receiveMessages(channel);
+        return ResponseEntity.ok(new ApiResponse<>(200 , "전송 성공" , null));
+    }
+
+    //실시간 통신 지원하는 용도인 듯
+    @PostMapping("addListener")
+    public ResponseEntity<ApiResponse<Void>> addListener(@RequestParam String channel){
+        pubnubPublishService.addListener(channel);
+        return ResponseEntity.ok(new ApiResponse<>(200,"수신 성공",null));
+    }
+
+
+
+
+
 
 
 
